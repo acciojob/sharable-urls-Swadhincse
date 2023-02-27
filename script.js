644063328f0296cce4c170e6cdd8edc5a16b25c0
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-02-28 01:22:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // your code here
-
 var form = document.querySelector("form");
 var h3 = document.querySelector("h3");
 var nameElement = document.querySelector("#name");
@@ -7,14 +6,14 @@ var nameElement = document.querySelector("#year");
 var btn = document.querySelector("#button");
 
 
-  function handleSubmit(event){
+function handleSubmit(event){
     event.preventDefault();
     var h3Value = 'https://localhost:8080/';
     var name = nameElement.value;
     var year = yearElement.value;
 
-    if(name && year) {
-        h3Value += '?name=' + name + '?year' +year;
+     if(name && year) {
+        h3Value += '?name=' + name + '&year=' +year;
     }
     else if(name && !year){
         h3Value += '?name=' + name;
